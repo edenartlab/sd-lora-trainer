eaaab69d8b18fd8cea0d29f7c7579416b016baab
mayukhdeb <mayukhmainak2000@gmail.com>
2024-03-31 04:47:16 -0700

    move seed fn

---
+++ trainer/utils/seed.py
@@ -0,0 +1,9 @@
+import numpy as np
+import random
+import torch
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
