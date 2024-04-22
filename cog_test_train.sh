327339ebbaa3561fe50bd383769a5299da1bd36e
aiXander <xandersteenbruggex@gmail.com>
2024-04-22 08:41:00 -0700

    prep for cog push

--- cog_test_train.sh
+++ cog_test_train.sh
@@ -2,12 +2,12 @@
 GPU_ID="device=2"
 
 cog predict --gpus $GPU_ID \
-    -i run_name="clipx_sdxl_cog" \
+    -i name="clipx_sdxl_cog" \
     -i caption_prefix="in the style of TOK, " \
     -i concept_mode="style" \
     -i sd_model_version="sdxl" \
-    -i max_train_steps="500" \
-    -i checkpointing_steps="125" \
+    -i max_train_steps="400" \
+    -i checkpointing_steps="500" \
     -i debug="False" \
     -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/clipx_tiny.zip" \
     -i seed="0"
